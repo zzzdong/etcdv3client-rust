cfa0c8955a55d23b21ff2efa44b1d81f119966d8
zzzdong <kuwater@163.com>
2019-02-27 16:20:48 +0800

    add get_with_prefix

--- src/kv.rs
+++ src/kv.rs
@@ -15,6 +15,12 @@ pub use crate::pb::rpc_grpc::{KVClient, KV as Service};
 
 use crate::client::EtcdClientError;
 
+/// SimpleKV with String as key
+pub struct SimpleKV {
+    pub key: String,
+    pub value: Vec<u8>,
+}
+
 /// KVClient which use String as key
 pub struct SimpleKVClient {
     inner: KVClient,
@@ -84,6 +90,33 @@ impl SimpleKVClient {
             .map_err(EtcdClientError::GRPC)?;
 
         Ok(())
+    }
+
+    /// Get bytes with prefix
+    pub fn get_with_prefix(&self, prefix: &str) -> Result<Vec<SimpleKV>, EtcdClientError> {
+        let mut req = RangeRequest::new();
+        let mut end = prefix.as_bytes().to_vec();
+        end.push(0xFF);
+
+        req.set_key(prefix.as_bytes().to_vec());
+        req.set_range_end(end);
+
+        let resp = self
+            .inner
+            .range(grpc::RequestOptions::new(), req)
+            .wait_drop_metadata()
+            .map_err(EtcdClientError::GRPC)?;
+
+        let kv = resp
+            .get_kvs()
+            .iter()
+            .map(|kv| SimpleKV {
+                key: String::from_utf8_lossy(kv.get_key()).to_string(),
+                value: kv.get_value().to_vec(),
+            })
+            .collect();
+
+        Ok(kv)
     }
 
     pub fn get_str(&self, key: &str) -> impl Future<Item = String, Error = EtcdClientError> {
